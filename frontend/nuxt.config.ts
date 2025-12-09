08e2a8109df7ea5c02bfd04606f541f765ca59b5
Sodbileg Tugsbayr <sodbileg@chimege.com>
2025-12-09 19:14:26 +0800

    feat: added cd script

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -84,12 +84,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: "Smart Note",
-      templateParams: {
-        site: {
-          url: 'https://example.com',
-        },
-        separator: '-',
-      },
+      titleTemplate: "%s",
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1, viewport-fit=cover" },
